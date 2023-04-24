a437236548ab677c1b4b154dadee098ed070991d
Elias El Khoury <90423789+Khoury-e@users.noreply.github.com>
2023-04-24 19:37:46 +0300

    Update sign up page

--- lib/sign_up.dart
+++ lib/sign_up.dart
@@ -1,4 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+
+/*install url_launcher flutter package before running this code.
+  install it using terminal (by executing: flutter pub get url_launcher)
+  add then the version onto the pubspec.yaml file (under dependencies)
+  dependencies:
+  flutter:
+    sdk: flutter
+  cupertino_icons: ^1.0.2
+  url_launcher: ^6.1.10 */
 
 class SignUp extends StatelessWidget {
   const SignUp({Key? key, required this.title}) : super(key: key);
@@ -7,7 +17,83 @@ class SignUp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(title)),
+      backgroundColor: const Color.fromARGB(255, 247, 213, 145),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            margin: const EdgeInsets.all(40.0),
+            child: const Text(
+              "Welcome to Burger Blast!!",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 40.0,
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(10.0),
+            child: const SizedBox(
+              height: 40.0,
+              width: 300.0,
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Email',
+                  hintText: 'Enter your email',
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(10.0),
+            child: const SizedBox(
+              height: 40.0,
+              width: 300.0,
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                  hintText: 'Enter Password',
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(10.0),
+            child: const SizedBox(
+              height: 40.0,
+              width: 300.0,
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Confirm Password',
+                  hintText: 'Confirm Your Password',
+                ),
+              ),
+            ),
+          ),
+          const Text("Or Sign Up using"),
+          Container(
+            margin: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                InkWell(
+                  child: Text("Facebook", style: TextStyle(color: Colors.blue)),
+                  onTap: () => launchUrlString('https://www.facebook.com'),
+                ),
+                InkWell(
+                  child: Text("LinkedIn", style: TextStyle(color: Colors.blue)),
+                  onTap: () => launchUrlString('https://www.linkedin.com'),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
