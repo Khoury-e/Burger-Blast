2276bbb1e94a2326e84c75f0aa08ad933700d5b7
Elias El Khoury <90423789+Khoury-e@users.noreply.github.com>
2023-04-24 14:50:42 +0300

    Updated the home page 2

--- lib/home_page2.dart
+++ lib/home_page2.dart
@@ -8,8 +8,108 @@ class HomePage2 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(title)),
-      body: Container(
-        color: Colors.black,
+      backgroundColor: const Color.fromARGB(255, 247, 213, 145),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            // add image here
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/view_menu');
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                foregroundColor: Colors.black,
+                padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                side: BorderSide(color: Colors.black, width: 2),
+              ),
+              child: Container(
+                width: 200,
+                margin: const EdgeInsets.all(16.0),
+                alignment: Alignment.center,
+                child: const Text(
+                  "View Menu",
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10.0),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/view_menu');
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                foregroundColor: Colors.black,
+                padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                side: BorderSide(color: Colors.black, width: 2),
+              ),
+              child: Container(
+                width: 200,
+                margin: const EdgeInsets.all(16.0),
+                alignment: Alignment.center,
+                child: const Text(
+                  "Special Offers",
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10.0),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/view_menu');
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                foregroundColor: Colors.black,
+                padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                side: BorderSide(color: Colors.black, width: 2),
+              ),
+              child: Container(
+                width: 200,
+                margin: const EdgeInsets.all(16.0),
+                alignment: Alignment.center,
+                child: const Text(
+                  "SIgn In/Sign Up",
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10.0),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/view_menu');
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                foregroundColor: Colors.black,
+                padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                side: BorderSide(color: Colors.black, width: 2),
+              ),
+              child: Container(
+                width: 200,
+                margin: const EdgeInsets.all(16.0),
+                alignment: Alignment.center,
+                child: const Text(
+                  "My Profile",
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
