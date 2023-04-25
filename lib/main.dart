9009444cc3891181b5287abe103122b6c90e903a
Elias El Khoury <90423789+Khoury-e@users.noreply.github.com>
2023-04-25 19:01:11 +0300

    Delete profile navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:burger_blast/menu.dart';
 import 'package:burger_blast/signin.dart';
 import 'package:flutter/material.dart';
-import 'package:burger_blast/Profile.dart';
 import 'package:burger_blast/sign_up.dart';
 import 'package:burger_blast/home_page2.dart';
 import 'package:burger_blast/home_page1.dart';
@@ -23,9 +22,6 @@ class MyApp extends StatelessWidget {
             const HomePage1(title: "Home Page 1"),
         '/home_2': (BuildContext context) =>
             const HomePage2(title: "Home Page 2"),
-        '/profile': (BuildContext context) => const Profile(
-              title: "My Profile",
-            ),
         '/view_menu': (BuildContext context) =>
             const ViewMenu(title: "View Menu"),
         '/signup': (BuildContext context) =>
