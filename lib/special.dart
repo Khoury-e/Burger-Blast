7268dad9539f66d549bccdf1cb11fd839c902bcb
Elias El Khoury <90423789+Khoury-e@users.noreply.github.com>
2023-04-26 20:17:55 +0300

    Added special offers page

---
+++ lib/special.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SpecialOffers extends StatelessWidget {
+  const SpecialOffers({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
